691c2db4e35f5f9ca15e0cad4fd3cfb6cfefd648
riverpearl <blebi2008@naver.com>
2015-11-27 17:10:55 +0900

    add UI, knowledgebase class

---
+++ MoveMove/MoveMove/OneRule.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MoveMove
+{
+    class OneRule : KnowledgeBase
+    {
+
+        public OneRule(string director, string season, string production, string genre, string grade, string distributor)
+            : base(director, season, production, genre, grade, distributor)
+        {
+            
+        }
+    }
+}
